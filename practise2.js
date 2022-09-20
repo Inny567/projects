5b209677b4da93d84b884d2ca604e616233f5c98
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-09-20 09:18:00 -0700

    new added feaatures

--- practise2.js
+++ practise2.js
@@ -155,7 +155,30 @@ const friend3 = 'Julius';
 const friends = ['Micheal', 'John', 'Julius'];
 console.log(friends);
 
-const years = new Array(1991, 1992, 1993, 1994, 1995);
+const y = new Array(1991, 1992, 1993, 1994, 1995);
 console.log(friends[0]);
 console.log(friends[1]);
 console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]); // This is to know the last element present in an Array
+friends[2] = 'King';
+console.log(friends);
+// friends = ['Bob', 'Alice']; //this cannot work because we cannot change the entire const values of an ARRAY.
+const firstName = 'Inioluwa';
+const vandross = [firstName, 'Emmanuel', 2022 - 2000, 'Developer', friends];
+console.log(vandross);
+console.log(vandross.length);
+
+//Exercises on Arrays
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years = [1991, 1967, 2002, 2010, 2018];
+const age1 = calcAge(years[0]);
+const age2 = calcAge(years[1]);
+const age3 = calcAge(years[years.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])];
+console.log(ages);
