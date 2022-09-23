579776fbb0e8810fb91f8b20b831946bc80acc05
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-09-23 11:04:24 -0700

    new added features

--- practise2.js
+++ practise2.js
@@ -408,6 +408,7 @@ if (mark.bmi > john.bmi) {
 //     console.log(`lifting weights repetition ${rep} 🏋️‍♀️`);
 // }
 
+/*
 // looping arrays, breaking and continuing
 const jonas = [
     'Jonas',
@@ -444,3 +445,18 @@ console.log(ages);
 
 // Continue and Break statement
 // Continue statement
+console.log('--- ONLY STRINGS ---');
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] !== 'string') continue;
+
+    console.log(jonas[i], typeof jonas[i]);
+}
+
+console.log('--- BREAK WITH NUMBER ---');
+for (let i = 0; i < jonas.length; i++) {
+    if (typeof jonas[i] === 'number') break;
+
+    console.log(jonas[i], typeof jonas[i]);
+}*/
+
+
