06fa925a01b0be5e7d1f32ed9e85651e2ac88637
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-09-28 09:25:22 -0700

    NEW FEATURES

--- prastise3.js
+++ prastise3.js
@@ -115,3 +115,27 @@ const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
 console.log(amplitudeBug);
 
 // CODING CHALLENGE #1
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+const temps1 = [17, 21, 23];
+const temps2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  let i = 0;
+  for (let i = 0; i < arr.length; i++);
+  return `"...${arr[i]}^C in ${arr[i].index + 1} days..."`;
+};
+
+console.log(printForecast(temps1));
+console.log(printForecast(temps2));
