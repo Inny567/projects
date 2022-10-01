b686ce1501e3885bac34fd997410fe13c3a8e8ab
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-01 07:08:45 -0700

    refactor .message

--- practise4/practise4.js
+++ practise4/practise4.js
@@ -11,13 +11,18 @@ document.querySelector('.guess').value = 0;
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   // When there is no input
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
+    displayMessage('⛔ No Number!');
 
     // when player wins
   } else if (guess === secretNumber) {
