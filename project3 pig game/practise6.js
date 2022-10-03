77afa6873a841f5ab01390217cf7e8b445fb40a7
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-03 11:36:04 -0700

    Rolling the dice and switching the active players of the PIG GAME

--- project3 pig game/practise6.js
+++ project3 pig game/practise6.js
@@ -1,9 +1,46 @@
 'use strict';
 
 // Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
-// console.log(score0El, score1El);
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+// starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
+diceEl.classList.add('hidden');
+
+const score = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+  // Display dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+  // Checked for rolled 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    currentScore = document.getElementById(
+      `current--${activePlayer}`
+    ).textContent = currentScore;
+  } else {
+    // Swith to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
