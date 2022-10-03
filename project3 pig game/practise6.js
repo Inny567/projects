45ea881aa924feec11055402e964ca4009e8f6a0
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-03 05:12:23 -0700

    pig game startup

---
+++ project3 pig game/practise6.js
@@ -0,0 +1,9 @@
+'use strict';
+
+// Selecting elements
+const score0El = document.getElementById('score--0');
+const score1El = document.getElementById('score--1');
+// console.log(score0El, score1El);
+
+score0El.textContent = 0;
+score1El.textContent = 0;
