1495c4b6fcc90ae260574ac4a3f28abc7f2a8ac5
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-10 03:44:37 -0700

    practice on primitives and objects works behind the scenes

--- behind the scenes of JS/practise7.js
+++ behind the scenes of JS/practise7.js
@@ -196,3 +196,55 @@ var addArrow = (a, b) => {
 addArrow(2, 5);
 addArrow(2, 5, 8); // this shows that in arrow functions  arguments is not defined.
 */
+
+// Primitive  vs  Objects (primitive vs reference types)
+// let age = 30;
+// let oldAge = age;
+// age = 31;
+// console.log(age);
+// console.log(oldAge);
+
+// const me = {
+//   name: 'Jonas',
+//   age: 30,
+// };
+// const friend = me;
+// friend.age = 27;
+// console.log('Friend', friend);
+// console.log('Me', me);
+
+// practice on primitive and objects
+// primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName);
+console.log(oldLastName);
+
+// reference types
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+
+console.log('Before marriage', jessica);
+console.log('After marriage', marriedJessica);
+
+// copying objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before marriage', jessica2);
+console.log('After marriage', jessicaCopy);
