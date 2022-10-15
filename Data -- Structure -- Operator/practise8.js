01e1dcea447e4e5c40a5122fb66e383c346391ad
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-15 04:33:11 -0700

    The FOR OF loop

--- Data -- Structure -- Operator/practise8.js
+++ Data -- Structure -- Operator/practise8.js
@@ -308,14 +308,24 @@ const restaurant = {
   },
 };
 
-// The NULLISH COALESCING Operator
-restaurant.numGuests = 0;
+// // The FOR OF lOOP
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+// for (const item of menu) console.log(item);
 
-const guests = restaurant.numGuests || 10;
-console.log(guests);
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+// console.log(...menu.entries());
+
+// // The NULLISH COALESCING Operator
+// restaurant.numGuests = 0;
+
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// const guestCorrect = restaurant.numGuests ?? 10;
+// console.log(guestCorrect);
 
 // Coding Challenge #1
 
@@ -337,46 +347,46 @@ TEST DATA FOR 6: Use players 'Davies', 'Muller', 'Lewandowski' and 'Kimmich'. Th
 GOOD LUCK 😀
 */
 
-const game = {
-  team1: 'Bayern Munich',
-  team2: 'Borrussia Dortmund',
-  players: [
-    [
-      'Neuer',
-      'Pavard',
-      'Martinez',
-      'Alaba',
-      'Davies',
-      'Kimmich',
-      'Goretzka',
-      'Coman',
-      'Muller',
-      'Gnarby',
-      'Lewandowski',
-    ],
-    [
-      'Burki',
-      'Schulz',
-      'Hummels',
-      'Akanji',
-      'Hakimi',
-      'Weigl',
-      'Witsel',
-      'Hazard',
-      'Brandt',
-      'Sancho',
-      'Gotze',
-    ],
-  ],
-  score: '4:0',
-  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
-  date: 'Nov 9th, 2037',
-  odds: {
-    team1: 1.33,
-    x: 3.25,
-    team2: 6.5,
-  },
-};
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrussia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alaba',
+//       'Davies',
+//       'Kimmich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lewandowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
 // 1
 
 // const players1 = [...game.players[0]];
@@ -385,21 +395,21 @@ const game = {
 // const players2 = [...game.players[1]];
 // console.log(players2);
 
-const [players1, players2] = game.players;
-console.log(players1, players2);
+// const [players1, players2] = game.players;
+// console.log(players1, players2);
 
-// 2.
-const [gk, ...fieldPlayers] = players1;
-console.log(fieldPlayers);
-console.log(gk);
+// // 2.
+// const [gk, ...fieldPlayers] = players1;
+// console.log(fieldPlayers);
+// console.log(gk);
 
-// 3
-const allPlayers = [...game.players[0], ...game.players[1]];
-console.log(allPlayers);
+// // 3
+// const allPlayers = [...game.players[0], ...game.players[1]];
+// console.log(allPlayers);
 
-// 4
-const players1Final = [...players1, ...['Thiago', 'Coutinho', 'Perisic']];
-console.log(players1Final);
+// // 4
+// const players1Final = [...players1, ...['Thiago', 'Coutinho', 'Perisic']];
+// console.log(players1Final);
 
 // 5
 // const team1 = game.odds.team1;
@@ -411,22 +421,22 @@ console.log(players1Final);
 // const team2 = game.odds.team2;
 // console.log(team2);
 
-const {
-  odds: { team1, x: draw, team2 },
-} = game;
-console.log(team1, draw, team2);
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+// console.log(team1, draw, team2);
 
-// 6
-const printGoals = function (...players) {
-  console.log(players);
-  console.log(`${players.length} goals ⚽ were scored`);
-};
-printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
-printGoals(...game.scored);
+// // 6
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals ⚽ were scored`);
+// };
+// printGoals('Davies', 'Muller', 'Lewandowski', 'Kimmich');
+// printGoals(...game.scored);
 
-// 7.
-team1 < team2 && console.log('Team1 is more likely to win');
-team1 > team2 && console.log('Team2 is more likely to win');
+// // 7.
+// team1 < team2 && console.log('Team1 is more likely to win');
+// team1 > team2 && console.log('Team2 is more likely to win');
 
 //REST PATTERN and PARAMETER
 /*
