57d37d484c0ef215909e51ab01cdac8c3350ec6f
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-18 10:39:10 -0700

    MAPS fundamentals

--- Data -- Structure -- Operator/practise8.js
+++ Data -- Structure -- Operator/practise8.js
@@ -312,6 +312,38 @@ const restaurant = {
   },
 };
 
+// // MAPS
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
+
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
+
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
+
+// // Examples
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear();
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.size);
+
+// const arr = [1, 2];
+// rest.set(arr, 'Test');
+// console.log(rest.get(arr));
+
 // // SETS
 // const ordersSet = new Set([
 //   'Pasta',
