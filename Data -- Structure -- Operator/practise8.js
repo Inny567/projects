d8b5601a7170f279a559e5fc43617462390cae32
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-18 02:10:17 -0700

    Looping Objects (object keys, values, and entries)

--- Data -- Structure -- Operator/practise8.js
+++ Data -- Structure -- Operator/practise8.js
@@ -312,6 +312,29 @@ const restaurant = {
   },
 };
 
+// // Looping Objects ( object.keys, values and entries)
+// // Looping over Property NAMES
+// const properties = Object.keys(openingHours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days:  `;
+// for (const day of properties) {
+//   openStr += `${day},  `;
+// }
+// console.log(openStr);
+
+// // Looping over Property VALUES
+// const values = Object.values(openingHours);
+// console.log(values);
+
+// // Entire Object ( ENTRIES)
+// const entries = Object.entries(openingHours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+
 // // Optional Chaining
 // if (restaurant.openingHours && restaurant.openingHours.mon)
 //   console.log(restaurant.openingHours.mon);
