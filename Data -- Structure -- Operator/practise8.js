0af38dbeb52f2bbfadbc1f94ec156cb6d335c47c
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-20 10:23:34 -0700

    working with strings - Part 2

--- Data -- Structure -- Operator/practise8.js
+++ Data -- Structure -- Operator/practise8.js
@@ -312,6 +312,83 @@ const restaurant = {
   },
 };
 
+// // Working With Strings - Part 2
+// const airline = 'TAP Air Portugal';
+
+// console.log(airline.toLowerCase());
+// console.log(airline.toUpperCase());
+
+// // Examples ( Fix capitalization in name)
+// const passenger = 'jOnAS'; //Jonas
+// const passengerLower = passenger.toLowerCase();
+// const passengerCorrect =
+//   passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// console.log(passengerCorrect);
+
+// // const fixName = function (passenger) {
+// //   const passengerLower = passenger.toLowerCase();
+// //   const passengerCorrect =
+// //     passengerLower[0].toUpperCase() + passengerLower.slice(1);
+// //   return passengerCorrect;
+// // };
+
+// // console.log(fixName('kElViN'));
+// // console.log(fixName('JULIUs'));
+
+// // Example ( Comparing user input email)
+// const email = 'hello@jonas.io';
+// const loginEmail = '  Hello@Jonas.Io \n';
+
+// // const lowerEmail = loginEmail.toLowerCase();
+// // const trimmedEmail = lowerEmail.trim();
+// // console.log(trimmedEmail);
+
+// const compareEmail = function (inputEmail) {
+//   const normalizedEmail = inputEmail.toLowerCase().trim();
+//   return email === normalizedEmail;
+// };
+// console.log(compareEmail('  Hello@Jonas.Io \n'));
+
+// const normalizedEmail = loginEmail.toLowerCase().trim();
+// console.log(normalizedEmail);
+// console.log(email === normalizedEmail);
+
+// // Example ( Replacing  Strings)
+// const priceGB = '288,97£';
+// const priceUS = priceGB.replace('£', '$').replace(',', '.');
+// console.log(priceUS);
+
+// const announcement =
+//   'All passengers come to bloarding door 23. Boarding door 23!';
+// console.log(announcement.replace('door', 'gate'));
+// console.log(announcement.replace(/door/g, 'gate'));
+
+// // Three simple method that returns boolean
+// // 1. includes
+// const plane = 'Airbus A320neo';
+// console.log(plane.includes('A320'));
+// console.log(plane.includes('neo'));
+// console.log(plane.startsWith('Air'));
+// console.log(plane.startsWith('Aib'));
+
+// if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+//   console.log('Part of the NEW Airbus family');
+// }
+
+// // Practice exercise
+// const checkBaggage = function (items) {
+//   const baggage = items.toLowerCase();
+//   if (baggage.includes('knife') || baggage.includes('gun')) {
+//     console.log('You are NOT allowed on board 😜');
+//   } else {
+//     console.log('Welcome aboard! 🎈🎈');
+//   }
+// };
+
+// checkBaggage('I have a laptop, some Food and a pocket Knife');
+// checkBaggage('Socks and Camera');
+// checkBaggage('Got some snacks and a Gun for protection');
+
 // // Working With Strings - Part 1
 // const airline = 'TAP Air Portugal';
 // const plane = 'A320';
