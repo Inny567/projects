ed3867a57e51621f91fa06573dfc9fbfd1c4cb36
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-20 19:36:12 -0700

    Working with string - Part 3

--- Data -- Structure -- Operator/practise8.js
+++ Data -- Structure -- Operator/practise8.js
@@ -312,6 +312,71 @@ const restaurant = {
   },
 };
 
+// // Working with Strings - part 3
+// // split METHOD
+// console.log('a+very+nice+string'.split('+'));
+// console.log('Emmanuel Inioluwa'.split(' '));
+
+// const [firstName, lastName] = 'Emmanuel Inioluwa'.split(' ');
+
+// // join METHOD
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// console.log(newName);
+
+// // Practical exercise
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
+
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '));
+// };
+
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('jonas schmedtmann');
+
+// // const name = 'jessica ann smith davis';
+// // const names = name.split(' ');
+// // const namesUpper = [];
+
+// // for (const n of names) {
+// //   namesUpper.push(n[0].toUpperCase() + n.slice(1));
+// // }
+
+// // const capitalizeName = namesUpper.join(' ');
+// // console.log(capitalizeName);
+
+// // PADDING a String
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(20, '+').padEnd(30, '+'));
+// console.log('jonas'.padStart(20, '+').padEnd(30, '+'));
+
+// // Example
+// const maskCreditCard = function (number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
+
+// console.log(maskCreditCard(983756726375735));
+// console.log(maskCreditCard(68346537357836));
+// console.log(maskCreditCard(4526725782547254));
+
+// // REPEAT Method
+// const message2 = 'Bad weather... All Departures Delayed... ';
+// console.log(message2.repeat(5));
+
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${'🛬'.repeat(n)}`);
+// };
+
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
+
 // // Working With Strings - Part 2
 // const airline = 'TAP Air Portugal';
 
