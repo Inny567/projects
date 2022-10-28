f44c938a88c8cf0859c16a7c7fbc35c47e51b2d2
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-28 16:29:05 -0700

    How To Pass Arguments into Functions

--- A Closer Look At Functions/Practise 9.js
+++ A Closer Look At Functions/Practise 9.js
@@ -27,3 +27,37 @@ createBooking('LH123', 2);
 createBooking('LH123', 5);
 createBooking('LH123', undefined, 1000); // skipping a parameter
 */
+
+/*
+const flight = 'LH234';
+const jonas = {
+  name: 'Emmanuel Inioluwa',
+  passport: 23957867597,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 23957867597) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// // Is the same as doing ...
+// const flightNum = flight;
+// const passenger = jonas;
+
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 100000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
