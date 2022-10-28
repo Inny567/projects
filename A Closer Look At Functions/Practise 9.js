110af9c6ca2b929b7fb9cbecd6cae1fd4d9be2b0
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-28 14:57:43 -0700

    Default Parameters

--- A Closer Look At Functions/Practise 9.js
+++ A Closer Look At Functions/Practise 9.js
@@ -1,1 +1,29 @@
 'use strict';
+
+/*
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  //   numPassengers = numPassengers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123', undefined, 1000); // skipping a parameter
+*/
