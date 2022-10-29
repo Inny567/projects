9d65c44fcc6f1258c67f718a828fdfcc7e365c3c
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-10-29 16:11:10 -0700

    Callbacks Functions

--- A Closer Look At Functions/Practise 9.js
+++ A Closer Look At Functions/Practise 9.js
@@ -61,3 +61,33 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 */
+
+/*
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher Order Function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by : ${fn.name}`);
+};
+
+transformer('Javascript is the best!', upperFirstWord);
+transformer('Javascript is the best!', oneWord);
+
+// JS uses callbacks all the times
+const high5 = function () {
+  console.log('🖐');
+};
+
+document.body.addEventListener('click', high5);
+
+['Jonas', 'Martha', 'Adam'].forEach(high5);
+*/
