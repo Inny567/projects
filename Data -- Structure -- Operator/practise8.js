272bd8bc56f381a17755a09f4159c91e4206c8db
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-12-16 05:21:21 -0800

    Coding Challenge 1

--- Data -- Structure -- Operator/practise8.js
+++ Data -- Structure -- Operator/practise8.js
@@ -347,6 +347,20 @@ document.body.append(document.createElement('textarea'));
 document.body.append(document.createElement('button'));
 const text = document.querySelector('textarea').value;
 
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+
+//   for (const row of rows) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(output);
+//   }
+// });
 // // Working with Strings - part 3
 // // split METHOD
 // console.log('a+very+nice+string'.split('+'));
