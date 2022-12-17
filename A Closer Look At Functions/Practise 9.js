c3d147dadcb8e8f12838e168384222c1384c9977
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-12-17 04:12:27 -0800

    More Examples on Closures

--- A Closer Look At Functions/Practise 9.js
+++ A Closer Look At Functions/Practise 9.js
@@ -361,3 +361,48 @@ booker();
 
 console.dir(booker);
 */
+
+// More Example on CLOSURE
+// Example 1
+
+/*
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+console.dir(f);
+
+// Re-assigning f Function
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassenger = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengrs`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassenger(180, 3);
+*/
