9a7481ba25fc22e95906f3e7981ca0d329135f3a
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-12-19 09:59:49 -0800

    The Map Method

--- Working with arrays/Practise10.js
+++ Working with arrays/Practise10.js
@@ -83,7 +83,7 @@ displayMovements(account1.movements);
 /////////////////////////////////////////////////
 // LECTURES
 
-// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -221,3 +221,31 @@ GOOD LUCK 😀
 
 // checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 // checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map(function (mov) {
+  return mov * eurToUsd;
+});
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDfor = [];
+for (const mov of movements) movementsUSDfor.push(mov * eurToUsd);
+console.log(movementsUSDfor);
+
+const movementsUSD1 = movements.map(mov => mov * eurToUsd);
+console.log(movementsUSD1);
+
+const movementsDescriptions = movements.map(
+  (mov, i) =>
+    `Movement ${i + 1}: You ${mov > 0 ? 'deposited' : 'withdrew'} ${Math.abs(
+      mov
+    )}`
+  //   if (mov > 0) {
+  //     return `Movement ${i + 1}: You deposited ${mov}`;
+  //   } else {
+  //     return `Movment ${i + 1}: You withdrew ${Math.abs(mov)}`;
+  //   }
+);
+console.log(movementsDescriptions);
