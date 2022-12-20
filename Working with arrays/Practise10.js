ea839899ba1bd61c36f3c562d5a4f44d9d98a9f0
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-12-19 16:11:04 -0800

    FILTER Method

--- Working with arrays/Practise10.js
+++ Working with arrays/Practise10.js
@@ -262,3 +262,19 @@ GOOD LUCK 😀
 //   //   }
 // );
 // console.log(movementsDescriptions);
+
+// // FILTER Method
+// const deposits = movements.filter(function (mov) {
+//   return mov > 0;
+// });
+// console.log(movements);
+// console.log(deposits);
+
+// const depositsFor = [];
+// for (const mov of movements) if (mov > 0) depositsFor.push(mov);
+// console.log(depositsFor);
+
+// const withdrawals = movements.filter(function (mov) {
+//   return mov < 0;
+// });
+// console.log(withdrawals);
