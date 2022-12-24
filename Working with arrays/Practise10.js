a441f2a7278fe2f9054216c06fcaa72022757319
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2022-12-23 16:39:56 -0800

    The Find Index Method & Implementing Close Accounts Function

--- Working with arrays/Practise10.js
+++ Working with arrays/Practise10.js
@@ -178,6 +178,26 @@ btnTransfer.addEventListener('click', function (e) {
   }
 });
 
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    console.log(index);
+
+    // Delete Account
+    accounts.splice(index, 1);
+
+    containerApp.style.opacity = 0;
+  }
+
+  inputCloseUsername.value = inputClosePin.value = '';
+  inputClosePin.blur();
+});
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
