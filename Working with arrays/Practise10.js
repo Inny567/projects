c4ad537a49de04acef1532baa27dc3ccbe0971dc
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-01-04 15:11:28 -0800

    FLAT and FlatMap Method

--- Working with arrays/Practise10.js
+++ Working with arrays/Practise10.js
@@ -524,3 +524,37 @@ TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
 // console.log(movements.some(deposit));
 // console.log(movements.every(deposit));
 // console.log(movements.filter(deposit));
+
+// FLAT METHOD
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+console.log(arrDeep.flat(2));
+
+// const accountMovements = accounts.map(acc => acc.movements);
+// console.log(accountMovements);
+
+// const allMovements = accountMovements.flat();
+// console.log(allMovements);
+
+// const overalBalance = allMovements.reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance);
+
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance);
+
+// FLATMAP Method
+const allMovements2 = accounts.flatMap(acc => acc.movements);
+console.log(allMovements2);
+
+// const overalBalance2 = allMovements2.reduce((acc, mov) => acc + mov, 0);
+// console.log(overalBalance2);
+
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+console.log(overalBalance2);
