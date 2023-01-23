59f65962b98b96fbaed659d2164fff4537823a39
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-01-23 10:14:10 -0800

    Converting and Checking Numbers

--- 12-Numbers-Dates, Timers - BANKIST APP/practise11.js
+++ 12-Numbers-Dates, Timers - BANKIST APP/practise11.js
@@ -164,7 +164,7 @@ btnLogin.addEventListener('click', function (e) {
   );
   console.log(currentAccount);
 
-  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+  if (currentAccount?.pin === +inputLoginPin.value) {
     // Display UI and message
     labelWelcome.textContent = `Welcome back, ${
       currentAccount.owner.split(' ')[0]
@@ -182,7 +182,7 @@ btnLogin.addEventListener('click', function (e) {
 
 btnTransfer.addEventListener('click', function (e) {
   e.preventDefault();
-  const amount = Number(inputTransferAmount.value);
+  const amount = +inputTransferAmount.value;
   const receiverAcc = accounts.find(
     acc => acc.username === inputTransferTo.value
   );
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = Number(inputLoanAmount.value);
+  const amount = +inputLoanAmount.value;
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -223,7 +223,7 @@ btnClose.addEventListener('click', function (e) {
 
   if (
     inputCloseUsername.value === currentAccount.username &&
-    Number(inputClosePin.value) === currentAccount.pin
+    +inputClosePin.value === currentAccount.pin
   ) {
     const index = accounts.findIndex(
       acc => acc.username === currentAccount.username
@@ -251,3 +251,42 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+
+// console.log(23 === 23.0);
+
+// // Base 10 - 0 to 9
+// // Binary base 2 - 0,1
+
+// console.log(0.1 + 0.2);
+// console.log(0.1 + 0.2 === 0.3);
+
+// // CONVERSION OF STRINGS TO NUMBERS
+// console.log(Number('23'));
+// console.log(+'23');
+
+// // PARSING
+// console.log(Number.parseInt('30px', 10));
+// console.log(Number.parseInt('e30', 10));
+
+// console.log(Number.parseFloat('3.4rem'));
+// console.log(Number.parseInt('3.4rem'));
+
+// //console.log(parseFloat(' 2.5rem '));
+
+// // Check if avalue is not a number
+// console.log(Number.isNaN(20));
+// console.log(Number.isNaN('20x'));
+// console.log(Number.isNaN(+'20x'));
+// console.log(Number.isNaN(23 / 0));
+
+// // Checking if value is a number
+// console.log(Number.isFinite(20));
+// console.log(Number.isFinite('20'));
+// console.log(Number.isFinite(+'20x'));
+// console.log(Number.isFinite(23 / 0));
+
+// // Checking if value is an integer
+// console.log(Number.isInteger(23));
+// console.log(Number.isInteger(23.0));
+// console.log(Number.isInteger(23 / 0));
+// console.log(Number.isInteger('23'));
