cd4d2a9db383770fe874f638850c792fb48d1247
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-01-23 14:04:55 -0800

    Math and Rounding

--- 12-Numbers-Dates, Timers - BANKIST APP/practise11.js
+++ 12-Numbers-Dates, Timers - BANKIST APP/practise11.js
@@ -94,7 +94,7 @@ const displayMovements = function (movements, sort = false) {
         <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-        <div class="movements__value">${mov}€</div>
+        <div class="movements__value">${mov.toFixed(2)}€</div>
       </div>
     `;
 
@@ -104,19 +104,19 @@ const displayMovements = function (movements, sort = false) {
 
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
-  labelBalance.textContent = `${acc.balance}€`;
+  labelBalance.textContent = `${acc.balance.toFixed(2)}€`;
 };
 
 const calcDisplaySummary = function (acc) {
   const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumIn.textContent = `${incomes}€`;
+  labelSumIn.textContent = `${incomes.toFixed(2)}€`;
 
   const out = acc.movements
     .filter(mov => mov < 0)
     .reduce((acc, mov) => acc + mov, 0);
-  labelSumOut.textContent = `${Math.abs(out)}€`;
+  labelSumOut.textContent = `${Math.abs(out.toFixed(2))}€`;
 
   const interest = acc.movements
     .filter(mov => mov > 0)
@@ -126,7 +126,7 @@ const calcDisplaySummary = function (acc) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `${interest}€`;
+  labelSumInterest.textContent = `${interest.toFixed(2)}€`;
 };
 
 const createUsernames = function (accs) {
@@ -206,7 +206,7 @@ btnTransfer.addEventListener('click', function (e) {
 btnLoan.addEventListener('click', function (e) {
   e.preventDefault();
 
-  const amount = +inputLoanAmount.value;
+  const amount = Math.floor(inputLoanAmount.value).toFixed(2);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
     // Add movement
@@ -290,3 +290,46 @@ btnSort.addEventListener('click', function (e) {
 // console.log(Number.isInteger(23.0));
 // console.log(Number.isInteger(23 / 0));
 // console.log(Number.isInteger('23'));
+
+// // Math and Rounding
+// // sqrt operator
+// console.log(Math.sqrt(25));
+
+// // Exponential operator
+// console.log(25 ** (1 / 2));
+// console.log(8 ** (1 / 3));
+
+// // max operator
+// // const arr = [1, 8, 90, 6, 600, 77];
+// // console.log(Math.max(...arr));
+// console.log(Math.max(5, 35, 90, 36, 0));
+
+// // min operator
+// console.log(Math.min(5, 35, 90, 36, 0));
+
+// // CONSTANTS ON MATH OBJECT
+// // 1. The PI function
+// console.log(Math.PI * Number.parseFloat('10px') ** 2);
+
+// // 2. The random function
+// console.log(Math.trunc(Math.random() * 6) + 1);
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min) + 1) + min;
+// // 0...1 -> 0... (max -min) -> min...max
+// // console.log(randomInt(10, 20));
+
+// // Rounding Integers
+// console.log(Math.trunc(23.3));
+
+// console.log(Math.round(23.3));
+// console.log(Math.round(23.9));
+
+// console.log(Math.ceil(23.3));
+// console.log(Math.ceil(23.9));
+
+// console.log(Math.floor(23.3));
+// console.log(Math.floor(23.9));
+
+// console.log(Math.trunc(-23.3));
+// console.log(Math.floor(-23.3));
