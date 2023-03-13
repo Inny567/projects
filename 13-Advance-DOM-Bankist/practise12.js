8ccf1e8ad3f1e6741db98a36283a196e004256d9
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-03-13 08:15:14 -0700

    Implementing Smooth Scrolling

--- 13-Advance-DOM-Bankist/practise12.js
+++ 13-Advance-DOM-Bankist/practise12.js
@@ -108,3 +108,35 @@ document.addEventListener('keydown', function (e) {
 
 // // Don't Use
 // //logo.className = 'INNY';
+
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function (e) {
+  const s1coords = section1.getBoundingClientRect();
+  console.log(s1coords);
+
+  console.log(e.target.getBoundingClientRect());
+
+  console.log('Current Scroll (X/Y)', window.pageXOffset, window.pageYOffset);
+
+  console.log(
+    'height/width viewport',
+    document.documentElement.clientHeight,
+    document.documentElement.clientWidth
+  );
+
+  // SCROLLING
+  // window.scrollTo(
+  //   s1coords.left + window.pageXOffset,
+  //   s1coords.top + window.pageYOffset
+  // );
+
+  // window.scrollTo({
+  //   left: s1coords.left + window.pageXOffset,
+  //   top: s1coords.top + window.pageYOffset,
+  //   behaviour: 'smooth',
+  // });
+
+  section1.scrollIntoView({ behaviour: 'smooth' });
+});
