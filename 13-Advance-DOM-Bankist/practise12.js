ef1664a3aa3214c62bfa22a3365ef94207283c49
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-06-21 08:13:19 -0700

    EVENT PROPAGATION

--- 13-Advance-DOM-Bankist/practise12.js
+++ 13-Advance-DOM-Bankist/practise12.js
@@ -153,3 +153,32 @@ btnScrollTo.addEventListener('click', function (e) {
 // h1.onmouseenter = function (e) {
 //   alert('onmouseenter : Great! you are reading the Heading');
 // };
+
+// // EVENT PROPAGATION
+// // rgb(255,255,255)
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+// // console.log(randomColor(0, 255));
+
+// document.querySelector('.nav__link').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+//   console.log(e.currentTarget === this);
+
+//   // STOP PROPAGATION
+//   // e.stopPropagation();
+// });
+// document.querySelector('.nav__links').addEventListener('click', function (e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('CONTAINER', e.target, e.currentTarget);
+// });
+// document.querySelector('.nav').addEventListener(
+//   'click',
+//   function (e) {
+//     this.style.backgroundColor = randomColor();
+//     console.log('NAV', e.target, e.currentTarget);
+//   },
+//   true
+// );
