95a9e38019b1aced0e7e900ee38a42088dedf6d4
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-06-22 10:19:05 -0700

    DOM Tranverse

--- 13-Advance-DOM-Bankist/practise12.js
+++ 13-Advance-DOM-Bankist/practise12.js
@@ -203,3 +203,34 @@ document.querySelector('.nav__link').addEventListener('click', function (e) {
 //   this.style.backgroundColor = randomColor();
 //   console.log('NAV', e.target, e.currentTarget);
 // });
+
+// // DOM TRANVERSNG
+
+// const h1 = document.querySelector('h1');
+
+// // GOING DOWNWARDS: CHILD
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
+// h1.firstElementChild.style.color = 'white';
+// h1.lastElementChild.style.color = 'orangered';
+
+// //GOING UPWARDS: PARENTS
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
+
+// h1.closest('.header').style.background = 'var(--gradient-secondary)';
+
+// h1.closest('h1').style.background = 'var(--gradient-primary)';
+
+// // GOING SIDEWAYS: SIBLINGS
+// console.log(h1.previousElementSibling);
+// console.log(h1.nextElementSibling);
+
+// console.log(h1.previousSibling);
+// console.log(h1.nextSibling);
+
+// console.log(h1.parentElement.children);
+// [...h1.parentElement.children].forEach(function (el) {
+//   if (el !== h1) el.style.transform = 'scale(0.5)';
+// });
