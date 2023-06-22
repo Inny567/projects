4d4449441b38c91579a7d95c32bf2bd92b203a0e
iamemmanuelinioluwa <emmyvandross567@gmail.com>
2023-06-22 09:10:34 -0700

    Event propagation & implementing page navigation

--- 13-Advance-DOM-Bankist/practise12.js
+++ 13-Advance-DOM-Bankist/practise12.js
@@ -11,6 +11,28 @@ const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 btnScrollTo.addEventListener('click', function (e) {
   const s1coords = section1.getBoundingClientRect();
   console.log(s1coords);
@@ -40,25 +62,28 @@ btnScrollTo.addEventListener('click', function (e) {
   section1.scrollIntoView({ behaviour: 'smooth' });
 });
 
-const openModal = function (e) {
+/////////////////////
+/// PAGE NAVIGATION
+
+// document.querySelectorAll('.nav__link').forEach(function (el) {
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//      document.querySelector(id).scrollIntoView({ behaviour: 'smooth' });
+//   });
+// });
+
+// 1. Add Event listeners to common parent element
+// 2. Determine what element origiated the event
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
   e.preventDefault();
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
-};
 
-const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
-};
-
-btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
-
-btnCloseModal.addEventListener('click', closeModal);
-overlay.addEventListener('click', closeModal);
-
-document.addEventListener('keydown', function (e) {
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  // MATCHING STRATEGY
+  if (e.target.classList.contains('nav__link')) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behaviour: 'smooth' });
   }
 });
 
